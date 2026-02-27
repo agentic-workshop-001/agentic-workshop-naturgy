e6efa462229e335909edef81128d6d831da3d008
Simon Serrano <serrano1004@gmail.com>
2026-02-27 11:10:09 +0100

    fix: private S3 + presigned URLs (IAM lacks CloudFront/public S3 perms)

--- terraform/reports/outputs.tf
+++ terraform/reports/outputs.tf
@@ -2,13 +2,3 @@ output "reports_bucket_name" {
   description = "Name of the S3 bucket hosting the test reports"
   value       = aws_s3_bucket.reports.id
 }
-
-output "reports_url" {
-  description = "Public URL to access the reports website via CloudFront"
-  value       = "https://${aws_cloudfront_distribution.reports.domain_name}"
-}
-
-output "cloudfront_distribution_id" {
-  description = "CloudFront distribution ID (for cache invalidation)"
-  value       = aws_cloudfront_distribution.reports.id
-}
